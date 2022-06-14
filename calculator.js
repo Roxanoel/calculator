08338ce04f246b7f385854687c2ea9115cca8a7c
Roxanoel <roxane.noel92@gmail.com>
2022-06-14 10:19:36 +0100

    Created and tested the four basic functions for adding, subtracting, multiplying, or dividing two numbers.

---
+++ calculator.js
@@ -0,0 +1,24 @@
+//#region Basic operator functions
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+function subtract(num1, num2) {
+    return num1 - num2;
+}
+
+function multiply(num1, num2) {
+    return num1*num2;
+}
+
+function divide(num1, num2) {
+    if (num2 === 0) {
+        // Desired behaviour tbd with rest of the functionality; atm, returns undefined.
+        alert("Hey! You can't divide by zero!");
+    }
+    else {
+        return num1/num2;
+    }
+
+}
+//#endregion
