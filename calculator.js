351a7ff714bb5a18cfeb866f465c5683a2d81aff
Roxanoel <roxane.noel92@gmail.com>
2022-06-15 10:26:21 +0100

    Changed the code to display an error message when input is too long for the display

--- calculator.js
+++ calculator.js
@@ -145,7 +145,21 @@ function checkIfOperator(element) {
 function updateDisplay() {
     // Updates display to current state of the array, turned to string
     equationAsString = valuesToOperateOn.join("");
-    display.innerText = equationAsString;
+    // Ensures the size of the string that is displayed fits the screen
+    const resized = resizeString(equationAsString, 18);
+
+    display.innerText = resized;
+} 
+
+function resizeString(string, maxLength) {
+    if (string.length <= maxLength) return string;
+
+    // If the string is longer than the max length, remove chars from the start
+    //const substring = string.substr(0, (string.length) - maxLength)
+    //return string.replace(substring, "");
+
+    // Replacing the string with an error message
+    return "Error: input too long"
 }
 
 function clear() {
